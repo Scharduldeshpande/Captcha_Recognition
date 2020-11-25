585383d3ae34e2eb08362c1385d9a2781374b6ed
Shardul Deshpande <35310690+Scharduldeshpande@users.noreply.github.com>
2020-11-25 15:50:53 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-DATA_DIR = "input/captcha_images_v2/"
+DATA_DIR = "input/training_data/"
 BATCH_SIZE = 8
 IMAGE_WIDTH = 300
 IMAGE_HEIGHT = 75
 NUM_WORKERS = 0
 EPOCHS = 2500
-DEVICE = "cpu"
+DEVICE = "cpu"       # Change to CUDA to train using a GPU
 RESULTS = "results"
